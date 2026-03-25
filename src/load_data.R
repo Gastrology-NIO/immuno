28135a225543748e54b83c24bbe90b92c14ac57a
Joanna <jm.ziemska@gmail.com>
2026-03-25 14:20:38 +0100

    Create load_data.R

---
+++ src/load_data.R
@@ -0,0 +1,22 @@
+
+load_data_meta<- function(path_metadata){
+  metadata<-read.csv(path_metadata, sep=";")
+  #metadata_tmp<-metadata[c(metadata$type %in% c("Pobranie1", "kontrola")),]
+  metadata$probe_name<-gsub("S15156", "RNA_S15156", metadata$probe_name)
+  return(metadata)
+}
+
+load_data_counts<- function(path_htseq, gene_no_count_save_file){
+  experiments_all<-data.frame()
+  experiments_all<-read.csv(paste(path_htseq,metadata_tmp$probe_name[1], ".txt", sep=""),header=FALSE, sep="\t")
+  names(experiments_all)<-c('gene', metadata_tmp$probe_name[1])
+  for (i in metadata_tmp$probe_name[2:length(metadata_tmp$probe_name)]){
+    exp_id<-i
+    experiments<-read.csv(paste(path_htseq,i, ".txt", sep=""),header=FALSE, sep="\t")
+    names(experiments)<-c('gene', exp_id)
+    experiments_all<-merge(experiments_all, experiments)
+  }
+  write.csv2(experiments_all, gene_no_count_save_file)
+  return(experiments_all)
+}
+  
