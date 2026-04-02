0a38d1cae162246dd374802d5eab7c0be731e7e0
Joanna <jm.ziemska@gmail.com>
2026-04-02 12:19:06 +0200

    Update limma.R

--- src/limma.R
+++ src/limma.R
@@ -1,11 +1,13 @@
 
 
-run_limma<-function(x, output_file){
-    mm <- model.matrix(~ s + research)
+run_limma<-function(x, metadata, output_file){
+    s<-metadata$s
+    research <- metadata$research
+    mm <- model.matrix(~ s + research, data=x)
     keep <- filterByExpr(x, design=mm)
-    keep[c("__no_feature", "__ambiguous", "__too_low_aQual", "__not_aligned", "__alignment_not_unique")] <-FALSE
+    # keep[c("__no_feature", "__ambiguous", "__too_low_aQual", "__not_aligned", "__alignment_not_unique")] <-FALSE
     d0<-x[keep,]
-    y <- voom(d0 , mm, plot = T)
+    y <- voom(d0 , mm, plot = F)
     fit <- lmFit(y, mm)
     contr <- makeContrasts(researchContol, levels = colnames(coef(fit)))
     tmp <- contrasts.fit(fit, contr)
