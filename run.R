dc99c21912b07f6631687a5bc587c1258652cb22
Joanna <jm.ziemska@gmail.com>
2026-04-02 12:19:02 +0200

    Update run.R

--- run.R
+++ run.R
@@ -2,14 +2,19 @@
 source("./src/load_data.R")
 
 
-metadata<-load_data_meta("./data/meta_jag.csv")
-gene_counts<-load_data_counts("./htseq/")
+metadata<-load_data_meta("./data/metadata_jag.csv")
+gene_counts<-load_data_counts("./htseq/", "./data/gene_counts.csv")
 
 
 # 1 pobranie (3 miesiące vs powyżej 2 lat)
-conditions <- list(type='Pobranie 1', 'time of OS'=c('poniżej 3 miesięcy', '> 2 lata'))
+conditions <- list(type='Pobranie 1', 'time.of.OS'=c('poni\xbfej 3 miesi\xeacy', '> 2 lata'))
 metadata_1_analyse <- cut_metadata(metadata, conditions)
-metadata_1_analyse$research<-metadata_1_analyse$'time of OS'
+metadata_1_analyse$research<-metadata_1_analyse$'time.of.OS'
+metadata_1_analyse$research[metadata_1_analyse$research=='poni\xbfej 3 miesi\xeacy']<-"Control"
+metadata_1_analyse$research[metadata_1_analyse$research=='> 2 lata']<-"Searched"
+x<-load_DGE(metadata_1_analyse,  "./htseq/") 
+output_file<-"./result/1pobranie_3miesiace_vs_powyżej_2_lata.csv"
+run_1<-run_limma(x, metadata_1_analyse, output_file)
 
 # 1 pobranie (I linia vs kolejna linia)
 conditions <- list(type='Pobranie 1', 'linia'=c('I linia', ''))
