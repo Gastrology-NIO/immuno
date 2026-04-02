5b116211fac52238b01cd629580573b1cf63118d
Joanna <jm.ziemska@gmail.com>
2026-04-02 14:16:13 +0200

    Update run.R

--- run.R
+++ run.R
@@ -34,13 +34,13 @@ metadata_3_analyse <- cut_metadata(metadata, conditions)
 metadata_3_analyse$research<-metadata_3_analyse$type
 metadata_3_analyse$research[metadata_3_analyse$research=='Pobranie 1']<-"Control"
 metadata_3_analyse$research[metadata_3_analyse$research=='Pobranie 2']<-"Searched"
-metadata_3_analyse<- metadata_3_analyse[!c(metadata_3_analyse$probe_name %in% c("123IM", "96IM",  "95IM", "107IM", "92IM" , "116IM", "117IM", "110IM", "130IM","27IM",
-"34IM","36IM","33IM","39IM","43IM","35IM","64IM","45IM","72IM","47IM","49IM","66IM","61IM","78IM","70IM","87IM","94IM")),]
+# metadata_3_analyse<- metadata_3_analyse[!c(metadata_3_analyse$probe_name %in% c("123IM", "96IM",  "95IM", "107IM", "92IM" , "116IM", "117IM", "110IM", "130IM","27IM",
+# "34IM","36IM","33IM","39IM","43IM","35IM","64IM","45IM","72IM","47IM","49IM","66IM","61IM","78IM","70IM","87IM","94IM")),]
 # samtools sort -n -o ./sorted/41IM.nameSorted.bam ./star/41IMAligned.sortedByCoord.out.bam
 # htseq-count --stranded=reverse -f bam -r name ./sorted/RNA_41IM.nameSorted.bam ./reference/omo_sapiens.GRCh38.99.gtf > ./htseq/41IM.txt
 
-htseq-count --stranded=reverse -f bam -r name ./sorted/117IM.nameSorted.bam ./reference/Homo_sapiens.GRCh38.99.gtf > ./htseq/117IM.txt &
-htseq-count --stranded=reverse -f bam -r name ./sorted/45IM.nameSorted.bam ./reference/Homo_sapiens.GRCh38.99.gtf > ./htseq/45IM.txt &
+# htseq-count --stranded=reverse -f bam -r name ./sorted/117IM.nameSorted.bam ./reference/Homo_sapiens.GRCh38.99.gtf > ./htseq/117IM.txt &
+# htseq-count --stranded=reverse -f bam -r name ./sorted/45IM.nameSorted.bam ./reference/Homo_sapiens.GRCh38.99.gtf > ./htseq/45IM.txt &
 
 x<-load_DGE(metadata_3_analyse,  "./htseq/") 
 output_file<-"./result/NtproBNP_0_pobranie_vs_2_pobranie.csv"
