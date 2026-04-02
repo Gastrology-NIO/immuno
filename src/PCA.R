b0fdb95011fa755bd670ff88e4557a32f004db27
Joanna <jm.ziemska@gmail.com>
2026-04-02 14:43:04 +0200

    Update PCA.R

--- src/PCA.R
+++ src/PCA.R
@@ -1,1 +1,23 @@
 
+
+
+plotPCA<function(x, save_path){
+  mds <- plotMDS(x, plot = FALSE)
+
+  df <- data.frame(
+    Dim1 = mds$x,
+    Dim2 = mds$y,
+    group = research
+  )
+  
+  library(ggplot2)
+  
+  p<-ggplot(df, aes(Dim1, Dim2, color = group)) +
+    geom_point(size = 4) +
+    theme_minimal()
+
+    ggsave(
+    save_path,
+    plot = p,
+  )
+}
