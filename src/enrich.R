5ef3bcd248a61535bc9d2d5a0e0fcf73ae0550fc
Joanna <jm.ziemska@gmail.com>
2026-04-03 14:12:55 +0200

    Update enrich.R

--- src/enrich.R
+++ src/enrich.R
@@ -1,1 +1,15 @@
-
+library(clusterProfiler)
+library(org.Hs.eg.db)
+enrichGO<-function( genes,aspect="BP"){
+  # MF
+  # BP
+  # CC
+    ego2 <- enrichGO(gene         = genes,
+                    OrgDb         = org.Hs.eg.db,
+                    keyType       = 'ENSEMBL',
+                    ont           = aspect,
+                    pAdjustMethod = "BH",
+                    pvalueCutoff  = 0.01,
+                    qvalueCutoff  = 0.05)
+    return(ego2)
+  }
