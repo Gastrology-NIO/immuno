1ed8308eb2f15e0c57feb6b7ffb7bce0dd12ac88
Joanna <jm.ziemska@gmail.com>
2026-04-23 13:05:09 +0200

    Update limma.R

--- src/limma.R
+++ src/limma.R
@@ -1,5 +1,14 @@
 
-
+get_voom<-function(x, metadata){
+    s<-metadata$s
+    research <- metadata$research
+    mm <- model.matrix(~ s + research, data=x)
+    keep <- filterByExpr(x, design=mm)
+    # keep[c("__no_feature", "__ambiguous", "__too_low_aQual", "__not_aligned", "__alignment_not_unique")] <-FALSE
+    d0<-x[keep,]
+    y <- voom(d0 , mm, plot = F)
+    return(y)
+}
 run_limma<-function(x, metadata, output_file){
     s<-metadata$s
     research <- metadata$research
