216e56693f6bce849c1cd918f73b3741b2e01339
Joanna <jm.ziemska@gmail.com>
2026-04-29 09:48:09 +0200

    Update limma.R

--- src/limma.R
+++ src/limma.R
@@ -25,7 +25,22 @@ run_limma<-function(x, metadata, output_file){
     write.csv2(top.table, output_file)
     return(top.table)
 }
-
+run_deseq2<-function(x, metadata){
+    s<-metadata$s
+    research <- metadata$research
+     mm <- model.matrix(~ s + research, data=x)
+    keep <- filterByExpr(x, design=mm)
+    keep[c("__no_feature", "__ambiguous", "__too_low_aQual", "__not_aligned", "__alignment_not_unique")] <-FALSE
+    d0<-x[keep,]
+    dds <- DESeqDataSetFromMatrix(countData = d0,
+                                  colData = metadata,
+                                  design= ~ s + research)
+    diagdds = DESeq(dds , test="Wald", fitType="parametric")
+    res = results(diagdds, cooksCutoff = FALSE)
+        res <- results(diagdds, contrast = c("research", "Searched", "Control"))
+    res<-as(res, "data.frame")
+    return(res)
+}
 
 sensitivity_limma <- function(v, metadata, outlier, design_formula = ~ s + research) {
 
