bb0dd9e633b4fc78610a2409eea4ed9b8dab2fae
Joanna <jm.ziemska@gmail.com>
2026-05-08 17:51:37 +0200

    Update PCA.R

--- src/PCA.R
+++ src/PCA.R
@@ -1,10 +1,27 @@
 
+library(vegan)
 
-
-plotPCA<-function(dds,metadata, save_path){
+plotPCA<-function(dds,metadata, save_path, model){
 
   vsd <- vst(dds, blind = FALSE)
-mat <- assay(vsd)
+  mat <- assay(vsd)
+  dist_mat <- dist(t(mat))
+  adonis_res<-adonis2(
+    model,
+    data = as.data.frame(colData(vsd)),
+  )
+  pval <- adonis_res$`Pr(>F)`[1]
+  
+  r2 <- adonis_res$R2[1]
+  label_text <- paste0(
+    "PERMANOVA\nR² = ",
+    round(r2, 3),
+    "\np = ",
+    signif(pval, 3)
+)
+  
+
+  
 pca <- prcomp(t(mat), scale. = FALSE)
 pcaData <- data.frame(
     PC1 = pca$x[,1],
@@ -16,14 +33,39 @@ percentVar <- round(
     100 * (pca$sdev^2 / sum(pca$sdev^2)),
     1
 )
-p<-ggplot(pcaData,
+p <- ggplot(pcaData,
        aes(PC1, PC2,
            color = research)) +
+
     geom_point(size = 4) +
+
+    stat_ellipse(
+      aes(fill = research),
+      geom = "polygon",
+      alpha = 0.2,
+      color = NA
+    ) +
+
+    geom_text_repel(
+        aes(label = patient_id),
+        size = 3
+    ) +
+
+    annotate(
+        "text",
+        x = Inf,
+        y = Inf,
+        label = label_text,
+        hjust = 1.1,
+        vjust = 1.5,
+        size = 5
+    ) +
+
     theme_minimal() +
+
     xlab(paste0("PC1: ", percentVar[1], "%")) +
     ylab(paste0("PC2: ", percentVar[2], "%"))
-    ggsave(
+  ggsave(
     save_path,
     plot = p,
   )
@@ -33,26 +75,6 @@ p<-ggplot(pcaData,
 #     batch = vsd$patient_id
 # )
   # pca <- prcomp(t(mat_corr))
-#   mds <- plotMDS(x, plot = FALSE)
 
-#   df <- data.frame(
-#     Dim1 = mds$x,
-#     Dim2 = mds$y,
-#     group = metadata$research, 
-#     label = metadata$probe_name,
-#         research = metadata$research
 
-#   )
-  
-#   library(ggplot2)
-  
-# p <- ggplot(df, aes(Dim1, Dim2, color = research)) +
-#   geom_point(size = 4) +
-#   geom_text(aes(label = label), vjust = -0.5) +
-#   theme_minimal()
-
-#     ggsave(
-#     save_path,
-#     plot = p,
-#   )
 }
